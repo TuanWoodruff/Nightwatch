a87c87e5065e55db01eb8ea196d83e7228618d64
Tuan <woodrufftuan@gmail.com>
2020-03-19 13:38:57 -0700

    Update firstTest.js

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,8 +1,23 @@
 module.exports = {
+    '@tags' : ['google'],
     'First test case' (browser){
+        const mainQuery = 'Elon Musk';
+        const mainQueryInputSelector = 'input[name="as_q"]';
+        const languageDropdownSelector = '#lr_button';
+        const languageDropdownValueSelector = '.goog-menuitem[value="lang_it"]';
+        const lastUpdateDropdown = '#as_qdr_button';
+        const lastUpdateDropdwnSelector = '.goog-menuitem[value="m"]';
+        const submitButton = '.jfk-button[type="submit"]';
+
         browser
-            .url('https://news.ycombinator.com/')
-            .waitForElementVisible('.hnname')
-            .assert.containsText(".hnname", "Hacker News");
+            .url('https://www.google.com/advanced_search')
+            .setValue(mainQueryInputSelector, mainQuery)
+            .click(languageDropdownSelector)
+            .click(languageDropdownValueSelector)
+            .click(lastUpdateDropdown)
+            .click(lastUpdateDropdwnSelector)
+            .click(submitButton)
+            .saveScreenshot('tests_output/google.png')
+
     }
 }
